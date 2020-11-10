240b03c909a4923c986bcf586503980d733c7fdd
longxd <longxdself@gmail.com>
2020-11-10 18:41:12 +0800

    删除Swift  package

--- iOS-learning-artifact/Pods/Target Support Files/Pods-SwiftUI-learning-artifact (iOS)/Pods-SwiftUI-learning-artifact (iOS)-frameworks.sh
+++ iOS-learning-artifact/Pods/Target Support Files/Pods-SwiftUI-learning-artifact (iOS)/Pods-SwiftUI-learning-artifact (iOS)-frameworks.sh
@@ -197,10 +197,14 @@ if [ -r "${ARTIFACT_LIST_FILE}" ]; then
 fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ObjectMapper/ObjectMapper.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/lottie-ios/Lottie.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ObjectMapper/ObjectMapper.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/lottie-ios/Lottie.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
